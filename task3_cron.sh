66608600ac2cedf045a51bdca9f1dc4eabec8da3
Oleksandr Proshchuk <oleksandr@vm1.freebsd.com>
2019-02-15 14:31:13 +0200

    check info - cron

--- task3_cron.sh
+++ task3_cron.sh
@@ -1,25 +1,28 @@
 #!/usr/local/bin/bash
-echo "Select what you one"
-echo "1) Number of CPU Cores: "
-echo "2) Disk Space: " 
-echo "3) RAM Size: "
-echo "4) The last users which were login on the host: "               
-echo "5) Number of active Python and Perl process"
-read choice;
 
-case $choice in
-    1) sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2;;
-    2) df -h;;
-    3) sysctl hw.physmem;;
-    4) last | head -4;;
-    5) ps | awk '{
+if [ $1 == "CPU" ]; then
+    echo "Number of CPU Cores: "
+    sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2
+elif [ $1 == "RAM" ]; then
+   echo "RAM Size: "..
+   sysctl hw.physmem
+elif [ $1 == "DiskSpace" ]; then
+    echo "Disk Space: "
+    df -h
+elif [ $1 == "users" ]; then
+    echo "The last users which were login on the host: "
+    last | head -4
+elif [ $1 == "process" ]; then
+    echo "Number of active Python and Perl process"
+    ps | awk '{
                 for(i=1; i<=NF; i++){
                     if(i == 5 && $i == "python")
                         py_count = py_count + 1;
                     else if (i == 5 && $i == "perl")
                         perl_count = perl_count + 1;
                 }
-            }; END { printf "Number of active Python process: %s\nNumber of active Perl process: %s\n", py_count, perl_count }';;
-    *) echo "Empty args";;
-esac
+            }; END { printf "Number of active Python process: %s\nNumber of active Perl process: %s\n", py_count, perl_count }'
+else
+    echo "Nice try!"
+fi
 
